80ad5e3703f48bb7986b109553aee4a889770732
iidiong <idong30@hotmail.com>
2020-03-30 00:53:59 -0400

    Woring code with temperature upated

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
 // Setup empty JS object to act as endpoint for all routes
-const projectData = async ( url = '', data = {})=>{
-    const response = await fetch(url)
-    console.log(data)
-    .then( response => {
-        if(response.ok){
-            return response;
-        }throw Error (response.statusText)
-    })
-    //.then (response =>  response.json())
-    // .then ((result) => {
-
-    //     date.innerText = newDate;
-    //     content.innerText = `${result.name}, US`
-    //     const tempToFarh = `${result.main.temp * 9/5 - 459.67}`;
-    //     temp.innerText = `${Number.parseInt(tempToFarh)} °F`;
-    //     console.log(result)
-    //     console.log(result.main.temp)
-    // })
-    .catch (error => console.log(`There was an error :`, error))
-};
+projectData = {};
 
 // Require Express to run server and routes
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // Start up an instance of app
 const app = express();
@@ -29,41 +11,42 @@ const port = 3000;
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static("website"));
+app.use(express.static('website'));
 
-app.get('/all', sendData);
+app.get("/all", sendData) ;
 
-function sendData (request, response) {
-//     const data = projectData()
-//   response.send();
-//     console.log(request.body);
-//     console.log(projectData);
+function sendData (request, response ){
+    response.send(projectData)
+};
+app.post("/add", addTemp);
 
+function callBack(req, res){
+    console.log(req.body)
+    res.send("POST recieved")
 };
 
-app.post("/all", getTemp);
+app.post("/addTemp", addTemp);
+
+function addTemp(req, res) {
+    projectData.temperature = req.body.temperature;
+    projectData.date = req.body.date;
+    projectData.user_response = req.body.user_response;
+    
+    console.log(projectData);
+    res.send("POST recieved");
+}
 
 // Setup Server
 app.listen(port, () => {
   console.log(`Listening for request on port ${port}!`);
 });
 
-function getTemp(req, res) {
-  const newEntry = {
-    zipcode: req.zipCode,
-    apikey: req.apiKey
-  };
-  console.log("================");
-  console.log(req.body.zipCode);
-  console.log(req.body.apiKey);
-  projectData(newEntry);
-//   console.log(projectData);
-}
-
-// app.get("/", (req, res) => res.send("Hello World!"));
+  
